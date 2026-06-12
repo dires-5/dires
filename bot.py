0a24f2bc939042ee88bfdbe3e697dfcb7cb54dd1
dires-5 <mulusew207@gmail.com>
2026-06-12 01:55:47 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,7 +44,7 @@ from pdf_extractor import extract_from_fayda_pdf
 # ══════════════════════════════════════════════════════════════
 #  ⚙️  CONFIG
 # ══════════════════════════════════════════════════════════════
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8152344764:AAE_IiYZZO9Bg__lXykuGD5YkNjq-zZ0KcQ")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8878628793:AAHeQh372w5GFdGx6PAK5SlY4Mw_bL19B48")
 ADMIN_USERNAME = "dhtechs_admin"
 ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID", "0"))
 ADMIN_TOKEN = os.getenv("ADMIN_TOKEN", "")
